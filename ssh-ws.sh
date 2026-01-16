bea92251e912258d8f8a9aa2a7e2d4586ac3dcc2
NDEVXLK <ndevslk@gmail.com>
2026-01-16 07:23:19 +0530

    Update ssh-ws.sh

--- ssh-ws.sh
+++ ssh-ws.sh
@@ -1,3 +1,3 @@
-sudo wget -q -O /usr/local/bin/ssh-ws https://raw.githubusercontent.com/ndevxlk/ssh-ws/main/ssh-ws
+sudo wget -q -O /usr/local/bin/ssh-ws https://raw.githubusercontent.com/ndevxlk/ssh-ws/refs/heads/main/ssh-ws
 sudo chmod +x /usr/local/bin/ssh-ws
 ssh-ws
