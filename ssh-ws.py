84170ed4976bf163a9e3410b94e8880aa2847e7f
ndevxlk <ndevslk@gmail.com>
2026-05-28 07:36:56 +0530

    Update ssh-ws.py

--- ssh-ws.py
+++ ssh-ws.py
@@ -1,150 +1,75 @@
 #!/usr/bin/python3
-import socket, threading, select, sys, getopt, time
-LISTENING_ADDR = '127.0.0.1'
-LISTENING_PORT = 3000
-BUFLEN = 4096 * 4
-TIMEOUT = 60
-DEFAULT_HOST = '127.0.0.1:22'
-PASS = ''
-RESPONSE = 'HTTP/1.1 101 Switching Protocols\r\n\r\n'
-class Server(threading.Thread):
-    def __init__(self, host, port):
-        threading.Thread.__init__(self)
-        self.running = False
-        self.host = host
-        self.port = port
-        self.threads = []
-        self.threadsLock = threading.Lock()
-    def run(self):
-        self.soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.soc.settimeout(2)
-        intport = int(self.port)
-        self.soc.bind((self.host, intport))
-        self.soc.listen(0)
-        self.running = True
-        try:
-            while self.running:
-                try:
-                    c, addr = self.soc.accept()
-                    c.setblocking(1)
-                except socket.timeout:
-                    continue
-                conn = ConnectionHandler(c, self)
-                conn.start()
-                self.addConn(conn)
-        finally:
-            self.running = False
-            self.soc.close()
-    def addConn(self, conn):
-        try:
-            self.threadsLock.acquire()
-            if self.running:
-                self.threads.append(conn)
-        finally:
-            self.threadsLock.release()
-    def removeConn(self, conn):
-        try:
-            self.threadsLock.acquire()
-            self.threads.remove(conn)
-        finally:
-            self.threadsLock.release()
-    def close(self):
-        try:
-            self.running = False
-            self.threadsLock.acquire()
-            threads = list(self.threads)
-            for c in threads:
-                c.close()
-        finally:
-            self.threadsLock.release()
-class ConnectionHandler(threading.Thread):
-    def __init__(self, socClient, server):
-        threading.Thread.__init__(self)
-        self.clientClosed = False
-        self.targetClosed = True
-        self.client = socClient
-        self.client_buffer = b''
-        self.server = server
-    def close(self):
-        try:
-            if not self.clientClosed:
-                self.client.shutdown(socket.SHUT_RDWR)
-                self.client.close()
-        except:
-            pass
-        finally:
-            self.clientClosed = True
-        try:
-            if not self.targetClosed:
-                self.target.shutdown(socket.SHUT_RDWR)
-                self.target.close()
-        except:
-            pass
-        finally:
-            self.targetClosed = True
-    def run(self):
-        try:
-            self.client_buffer = self.client.recv(BUFLEN)
-            hostPort = DEFAULT_HOST.encode('utf-8')
-            self.method_CONNECT(hostPort)
-        finally:
-            self.close()
-            self.server.removeConn(self)
-    def connect_target(self, host):
-        i = host.find(b':')
-        if i != -1:
-            port = int(host[i+1:])
-            host = host[:i]
-        else:
-            port = LISTENING_PORT
-        (soc_family, soc_type, proto, _, address) = socket.getaddrinfo(host.decode('utf-8'), port)[0]
-        self.target = socket.socket(soc_family, soc_type, proto)
-        self.targetClosed = False
-        self.target.connect(address)
-    def method_CONNECT(self, path):
-        self.connect_target(path)
-        self.client.sendall(RESPONSE.encode('utf-8'))
-        self.client_buffer = b''
-        self.doCONNECT()
-    def doCONNECT(self):
-        socs = [self.client, self.target]
-        count = 0
-        error = False
+import asyncio
+import socket
+try:
+    import uvloop
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+except:
+    pass
+LISTEN_IP = '127.0.0.1'
+LISTEN_PORT = 3000
+TARGET_IP = '127.0.0.1'
+TARGET_PORT = 22
+BUFFER_SIZE = 16384
+RESPONSE = (
+    b'HTTP/1.1 101 Switching Protocols\r\n'
+    b'\r\n'
+)
+async def pipe(reader, writer):
+    try:
         while True:
-            count += 1
-            (recv, _, err) = select.select(socs, [], socs, 3)
-            if err:
-                error = True
-            if recv:
-                for in_ in recv:
-                    try:
-                        data = in_.recv(BUFLEN)
-                        if data:
-                            if in_ is self.target:
-                                self.client.send(data)
-                            else:
-                                while data:
-                                    byte = self.target.send(data)
-                                    data = data[byte:]
-                            count = 0
-                        else:
-                            break
-                    except:
-                        error = True
-                        break
-            if count == TIMEOUT:
-                error = True
-            if error:
+            data = await reader.read(BUFFER_SIZE)
+            if not data:
                 break
-def main(host=LISTENING_ADDR, port=LISTENING_PORT):
-    server = Server(LISTENING_ADDR, LISTENING_PORT)
-    server.start()
-    while True:
+            writer.write(data)
+            await writer.drain()
+    except:
+        pass
+async def handle_client(client_reader, client_writer):
+    target_writer = None
+    try:
+        first_data = await client_reader.read(BUFFER_SIZE)
+        if not first_data:
+            return
+        target_reader, target_writer = await asyncio.open_connection(
+            TARGET_IP,
+            TARGET_PORT
+        )
+        sock = target_writer.get_extra_info('socket')
+        if sock:
+            sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+            sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        target_writer.write(first_data)
+        await target_writer.drain()
+        client_writer.write(RESPONSE)
+        await client_writer.drain()
+        await asyncio.gather(
+            pipe(client_reader, target_writer),
+            pipe(target_reader, client_writer)
+        )
+    except:
+        pass
+    finally:
         try:
-            time.sleep(2)
-        except KeyboardInterrupt:
-            server.close()
-            break
+            client_writer.close()
+            await client_writer.wait_closed()
+        except:
+            pass
+        if target_writer:
+            try:
+                target_writer.close()
+                await target_writer.wait_closed()
+            except:
+                pass
+async def main():
+    server = await asyncio.start_server(
+        handle_client,
+        LISTEN_IP,
+        LISTEN_PORT,
+        reuse_address=True,
+        reuse_port=True
+    )
+    async with server:
+        await server.serve_forever()
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
